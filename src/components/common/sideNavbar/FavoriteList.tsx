3bf0bdd270fb2daef15bccbaf45645331e4d12a7
JM Kim <kjmhs501@naver.com>
2025-07-02 17:57:10 +0900

    feat: 관심& 알림 목록이 없으면 나오는 컴포넌트도 UI 구현완료

--- src/components/common/sideNavbar/FavoriteList.tsx
+++ src/components/common/sideNavbar/FavoriteList.tsx
@@ -1,9 +1,39 @@
 const FavoriteList = () => {
   return (
     <>
-      <div>관심 리스트</div>
-      <div>관심 리스트</div>
-      <div>관심 리스트</div>
+      {/* 헤더 */}
+      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2">
+        <div className="font-semibold text-2xl">관심</div>
+      </div>
+
+      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2 text-sm">
+        최근 본
+      </div>
+      <div className="flex flex-col justify-center items-center min-h-[72px] border-b border-[#CBC4CF]">
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+      </div>
+      {/* 관심그룹 1*/}
+      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2 text-sm">
+        관심그룹 1
+      </div>
+      <div className="flex flex-col items-center justify-center min-h-[72px] border-b border-[#CBC4CF]">
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+      </div>
+
+      {/* 관심그루 2*/}
+      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2 text-sm">
+        관심그룹 2
+      </div>
+
+      <div className="flex flex-col items-center justify-center min-h-[72px] border-b border-[#CBC4CF]">
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+        <div>관심 리스트</div>
+      </div>
     </>
   );
 };
--- src/components/common/sideNavbar/NotificationPanel.tsx
+++ src/components/common/sideNavbar/NotificationPanel.tsx
@@ -1,3 +1,4 @@
+import NoNotificationList from "./NoNotificationList";
 import NotificationList from "./NotificationList";
 
 const NotificationPanel = () => {
@@ -6,32 +7,10 @@ const NotificationPanel = () => {
       className="fixed right-[97px] w-[400px] h-screen flex flex-col bg-[#FFF] overflow-auto
     shadow-[0px_1px_2px_0px_rgba(0,0,0,0.30),_0px_1px_3px_1px_rgba(0,0,0,0.15)]"
     >
-      {/* 헤더 */}
-      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2">
-        <div className="font-semibold text-2xl">알림</div>
-        <div className="text-sm">이번 주</div>
-      </div>
-
-      {/* 이번 주 */}
-      <div className="flex flex-col justify-center items-center min-h-[72px] border-b border-[#CBC4CF]">
-        <NotificationList />
-      </div>
-      {/* 이번 달 */}
-      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2 text-sm">
-        이번 달
-      </div>
-      <div className="flex flex-col items-center justify-center min-h-[72px] border-b border-[#CBC4CF]">
-        <NotificationList />
-      </div>
-
-      {/* 이전 활동 */}
-      <div className="w-[400px] flex flex-col px-6 pt-6 pb-2 gap-2 text-sm">
-        이전 활동
-      </div>
-
-      <div className="flex flex-col items-center justify-center min-h-[72px] border-b border-[#CBC4CF]">
-        <NotificationList />
-      </div>
+      {/* 알림 목록이 있으면 이걸 사용 */}
+      <NotificationList />
+      {/* 알림 목록이 없으면 이걸 사용 */}
+      {/* <NoNotificationList /> */}
     </div>
   );
 };
