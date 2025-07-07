b9d0b855f5bd6a3b241c1ec2ac421f5f8ff71952
atank3566 <atank3566@naver.com>
2025-07-08 01:27:55 +0900

    desigin: 마이페이지 사이드바

---
+++ src/components/mypage/MyProjects.tsx
@@ -0,0 +1,11 @@
+const MyProjects = () => {
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-4">프로젝트 목록</h2>
+      {/* 프로젝트 리스트 등을 여기에 */}
+      
+    </div>
+  );
+};
+
+export default MyProjects;
