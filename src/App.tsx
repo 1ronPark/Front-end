3cbbf841d066c5bebab272d72f7c94cdf8de24e1
atank3566 <atank3566@naver.com>
2025-07-09 00:40:13 +0900

    fix:깃병합후 사이드바 위치 재설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Layout } from "./components/layout/Layout";
+import { FormLayout } from "./components/layout/FormLayout";
 import "./App.css";
 
 // 주디: 최상단 Navbar 표시를 위해 임의로 경로를 설정하였습니다!
@@ -9,6 +10,7 @@ import { Projects } from "./pages/Projects";
 import { Members } from "./pages/member/Members";
 import { LightTalk } from "./pages/LightTalk";
 import { MyProfile } from "./pages/MyProfile";
+import { FormEdit } from "./pages/FormEdit";
 
 function App() {
   return (
@@ -19,6 +21,10 @@ function App() {
           <Route path="/members" element={<Members />} />
           <Route path="/lighttalk" element={<LightTalk />} />
           <Route path="/myprofile" element={<MyProfile />} />
+        </Route>
+
+        <Route path="/myprofile/edit" element={<FormLayout />}>
+          <Route index element={<FormEdit />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,11 +5,16 @@ import { Footbar } from "../common/footbar/Footbar";
 
 export const Layout = () => {
   return (
-    <div className="w-full min-h-screen bg-white flex flex-col">
-      <MainNavbar isLoggedIn={false} userName="홍길동" />
-      <SideNavbar />
-      <div className="flex-1 w-full">
-        <Outlet />
+    <div className="relative flex min-h-screen w-full bg-white">
+      <div className="flex flex-1 flex-col pr-[60px]">
+        <MainNavbar isLoggedIn={false} userName="홍길동" />
+        <main className="flex-1">
+          <Outlet />
+        </main>
+        <Footbar />
+      </div>
+      <div className="fixed right-0 top-0 z-30 h-screen w-[60px] border-l bg-white shadow-md">
+        <SideNavbar />
       </div>
     </div>
   );
