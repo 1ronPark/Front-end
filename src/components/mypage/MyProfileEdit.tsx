cfb8331a1a4762c273351ab17cd95d408ce9be00
JM Kim <kjmhs501@naver.com>
2025-07-08 16:51:21 +0900

    회원정보에서 회원정보 & 비밀번호 까지 완성

--- src/components/mypage/MyProfileEdit.tsx
+++ src/components/mypage/MyProfileEdit.tsx
@@ -3,7 +3,6 @@ const MyProfileEdit = () => {
     <div>
       <h2 className="text-xl font-bold mb-4">프로필 수정</h2>
       {/* 여기에 form 요소 등 추가 */}
-      
     </div>
   );
 };
--- src/pages/MyProfile.tsx
+++ src/pages/MyProfile.tsx
@@ -1,19 +1,32 @@
-import { useState } from 'react';
-import MypageSidebar from '../components/mypage/MypageSidebar';
-import MyInfo from '../components/mypage/MyInfo';
-import MyProfileEdit from '../components/mypage/MyProfileEdit';
-import MyProjects from '../components/mypage/MyProjects';
+import { useState } from "react";
+import MypageSidebar from "../components/mypage/MypageSidebar";
+import MyInfo from "../components/mypage/myInfo/MyInfo";
+import MyProfileEdit from "../components/mypage/MyProfileEdit";
+import MyProjects from "../components/mypage/MyProjects";
 
 export const MyProfile = () => {
-  const [activeTab, setActiveTab] = useState<'info' | 'edit' | 'projects'>('info');
+  const [activeTab, setActiveTab] = useState<"info" | "edit" | "projects">(
+    "info"
+  );
 
   return (
     <div className="flex">
       <MypageSidebar activeTab={activeTab} setActiveTab={setActiveTab} />
       <div className="flex-1 p-6">
-        {activeTab === 'info' && <MyInfo />}
-        {activeTab === 'edit' && <MyProfileEdit />}
-        {activeTab === 'projects' && <MyProjects />}
+        {activeTab === "info" && (
+          <MyInfo
+            //회원 정보에 필요한 props들
+            name="홍길동"
+            nickname="홍"
+            phone="010-1234-5678"
+            email="hong@hong.ac.kr"
+            univ="길동대학교"
+            mbti="INTJ"
+            intro="나는 홍길동이다"
+          />
+        )}
+        {activeTab === "edit" && <MyProfileEdit />}
+        {activeTab === "projects" && <MyProjects />}
       </div>
     </div>
   );
