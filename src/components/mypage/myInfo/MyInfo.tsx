c6ec5fd725276c8b33589645012d78c06880b22e
JM Kim <kjmhs501@naver.com>
2025-07-08 17:04:41 +0900

    회원정보 페이지 UI 완성

--- src/components/mypage/myInfo/MyInfo.tsx
+++ src/components/mypage/myInfo/MyInfo.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, DoorOpen } from "lucide-react";
 import editIcon from "../../../assets/icons/mypage/ic_edit.svg";
 import sample from "../../../assets/icons/mypage/sample_profile.png";
 
@@ -14,7 +14,7 @@ interface myProps {
 
 const MyInfo = (myProps: myProps) => {
   return (
-    <div className="w-[1280px]">
+    <div className="w-[1280px] flex flex-col gap-[40px]">
       {/* 회원정보 */}
       <div className="flex flex-col px-[160px] gap-6">
         {/* header */}
@@ -94,8 +94,14 @@ const MyInfo = (myProps: myProps) => {
         </div>
       </div>
       {/* 회원 탈퇴 */}
-      <div className="flex flex-col justify-center items-center px-[160px] gap-6">
-        회원탈퇴
+      <div className="flex flex-col px-[160px] gap-6">
+        <div className="flex justify-between">
+          <p className="headline-small-emphasis">회원 탈퇴</p>
+          <button className="flex justify-center items-center px-3 py-1.5 gap-1 hover:cursor-pointer">
+            <DoorOpen className="w-[20px] h-[20px] text-[#49454E]" />
+            <p className="label-large">탈퇴 하기</p>
+          </button>
+        </div>
       </div>
     </div>
   );
