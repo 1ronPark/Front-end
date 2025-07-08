66402d3593ee4b3ca53dda5d3064b27e4914b2b4
atank3566 <atank3566@naver.com>
2025-07-08 20:42:10 +0900

    design:상단Navbar, Sidebar영역 설정

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -5,14 +5,12 @@ import { Footbar } from "../common/footbar/Footbar";
 
 export const Layout = () => {
   return (
-    <div className="w-full min-h-screen bg-white flex flex-col pr-[65px]">  
+    <div className="w-full min-h-screen bg-white flex flex-col">
       <MainNavbar isLoggedIn={false} userName="홍길동" />
       <SideNavbar />
-
       <div className="flex-1 w-full">
         <Outlet />
       </div>
-      <Footbar />
     </div>
   );
 };
