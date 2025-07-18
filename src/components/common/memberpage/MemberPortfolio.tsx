e00e8413f86d0cca34fab5669ee8ce708d444784
hyewonimdang <hyehyekuk202@gmail.com>
2025-07-18 20:29:21 +0900

    [design] github 카드 UI 구현 완성

--- src/components/common/memberpage/MemberPortfolio.tsx
+++ src/components/common/memberpage/MemberPortfolio.tsx
@@ -14,7 +14,7 @@ const MemberPortfolio = () => {
         setNumPages(numPages);
     };
 
-    // ✅ 리사이징에 따라 너비 측정
+    // PDF 크기에 따라 너비 측정
     useEffect(() => {
         const resizeObserver = new ResizeObserver((entries) => {
         const width = entries[0].contentRect.width;
@@ -36,33 +36,49 @@ const MemberPortfolio = () => {
                         창업에 누구보다 진심입니다. 같이 열정을 불태워서 실제로 수익 창출까지 프로덕트를 성장시킬 팀원을 구합니다. 반드시 제가 1등합니다.창업에 누구보다 진심입니다. 같이 열정을 불태워서 실제로 수익 창출까지 프로덕트를 성장시킬 팀원을 구합니다. 반드시 제가 1등합니다.창업에 누구보다 진심입니다. 같이 열정을 불태워서 실제로 수익 창출까지 프로덕트를 성장시킬 팀원을 구합니다. 반드시 제가 1등합니다.창업에 누구보다 진심입니다. 같이 열정을 불태워서 실제로 수익 창출까지 프로덕트를 성장시킬 팀원을 구합니다. 반드시 제가 1등합니다.
                     </p>
                     
-                    {/* ✅ 반응형 PDF wrapper */}
-        <div
-          className="h-[700px] overflow-y-scroll border rounded-md px-4 py-2"
-          ref={containerRef}
-        >
-          <Document
-            file="/dev_portfolio.pdf"
-            onLoadSuccess={onDocumentLoadSuccess}
-          >
-            {numPages &&
-              Array.from({ length: numPages }, (_, index) => (
-                <Page
-                  key={`page_${index + 1}`}
-                  pageNumber={index + 1}
-                  width={containerWidth - 32} // padding 고려
-                  className="mb-8 shadow"
-                  renderTextLayer={false}
-  renderAnnotationLayer={false}
-                />
-              ))}
-          </Document>
-        </div>
+                    {/* 반응형 PDF wrapper */}
+                    <div
+                        className="h-[700px] overflow-y-scroll border rounded-md px-4 py-2"
+                        ref={containerRef}
+                    >
+                        <Document
+                            file="/dev_portfolio.pdf"
+                            onLoadSuccess={onDocumentLoadSuccess}
+                        >
+                            {numPages &&
+                                Array.from({ length: numPages }, (_, index) => (
+                                    <Page
+                                    key={`page_${index + 1}`}
+                                    pageNumber={index + 1}
+                                    width={containerWidth - 32}
+                                    className="mb-8 shadow"
+                                    renderTextLayer={false}
+                                    renderAnnotationLayer={false}
+                                    />
+                            ))}
+                        </Document>
+                    </div>
 
 
                     <div className="flex items-center flex-wrap gap-4 mt-12">
-                        <img src={ic_github} alt="깃허브" />
-                        <img src={ic_github} alt="깃허브" />
+                        <div className="w-[304px] rounded-lg border border-[#C8C5D0] overflow-hidden">
+                            <img src={ic_github} alt="깃허브" className="w-full h-auto" />
+                            <div className="bg-white py-2 px-4 flex flex-start title-medium-emphasis">
+                                일론박의 Github
+                            </div>
+                        </div>
+                        <div className="w-[304px] rounded-lg border border-[#C8C5D0] overflow-hidden">
+                            <img src={ic_github} alt="깃허브" className="w-full h-auto" />
+                            <div className="bg-white py-2 px-4 flex flex-start title-medium-emphasis">
+                                일론박의 Github
+                            </div>
+                        </div>
+                        <div className="w-[304px] rounded-lg border border-[#C8C5D0] overflow-hidden">
+                            <img src={ic_github} alt="깃허브" className="w-full h-auto" />
+                            <div className="bg-white py-2 px-4 flex flex-start title-medium-emphasis">
+                                일론박의 Github
+                            </div>
+                        </div>
                     </div>
                 </div>
         </section>
