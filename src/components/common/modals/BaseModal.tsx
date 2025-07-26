0403de965089e19b67d4d820e56f45563e610c0e
hyun-wjd <helencho0530@gmail.com>
2025-07-27 03:01:41 +0900

    프로젝트 지원하기 모달 완료

--- src/components/common/modals/BaseModal.tsx
+++ src/components/common/modals/BaseModal.tsx
@@ -22,16 +22,25 @@ const BaseModal = ({
 }: BaseModalProps) => {
   if (!visible) return null;
 
-//디버깅용
+  //디버깅용
   console.log(document.getElementById("modal-root"));
 
-
   return ReactDOM.createPortal(
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-      <div className="flex flex-col bg-white rounded-2xl p-8 w-[384px] text-center shadow-lg gap-14">
+    //sideNavbar & mainNavbar : z-50 / 말풍선 : z-30
+    <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-40">
+      <div className="absolute inset-0" onClick={onCancel}></div>
+      <div
+        className="flex flex-col bg-white rounded-2xl p-8 w-[384px] text-center shadow-lg gap-14 z-35"
+        onClick={(e) => e.stopPropagation()}
+      >
         <div className="headline-large-emphasis">
           {title && <span className="text-[#6C63EF]">{title}</span>}
-          {description && <span className="text-[#1C1B21] break-keep whitespace-normal"> {description}</span>}
+          {description && (
+            <span className="text-[#1C1B21] break-keep whitespace-normal">
+              {" "}
+              {description}
+            </span>
+          )}
         </div>
         {/* 설명 영역 or children */}
         <div className="flex flex-col p-4 gap-2">
@@ -39,14 +48,16 @@ const BaseModal = ({
           <button
             type="button"
             onClick={onConfirm}
-            className="w-full px-6 py-4 rounded-xl bg-[#5A5891] text-white title-medium-emphasis"
+            className="w-full px-6 py-4 rounded-xl bg-[#5A5891] text-white title-medium-emphasis
+            hover:bg-[#413f7c] transition-colors duration-200 cursor-pointer"
           >
             {confirmText}
           </button>
           <button
             type="button"
             onClick={onCancel}
-            className="w-full px-6 py-4 rounded-xl border border-[#CBC6D9] text-[#49454E] title-medium-emphasis"
+            className="w-full px-6 py-4 rounded-xl border border-[#CBC6D9] text-[#49454E] title-medium-emphasis
+            hover:bg-[#f7f4ff] transition-colors duration-200 cursor-pointer"
           >
             {cancelText}
           </button>
