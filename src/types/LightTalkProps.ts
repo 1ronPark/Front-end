0c0504559e3b323d3eb5c2787619bab0420b6374
JM Kim <kjmhs501@naver.com>
2025-07-28 18:32:55 +0900

    fix: 깃 병합을 위한 커밋

--- src/types/LightTalkProps.ts
+++ src/types/LightTalkProps.ts
@@ -5,7 +5,7 @@ export interface TalkCardProps {
   // role?: "디자이너" | "PM" | "Web" | "Android" | "ios" | "Server" | "마케팅";
   role: string; //역할 위에 내용으로 정해진 파트를 할지 그냥 string으로 할지 고민중
   univ: string; //학교 이름
-  content: string; //글내용
+  // content: string; //글내용
   text: string; //글내용
   images: string[]; //이미지 리스트
   createAt: Date; //글이 작성된 날짜
