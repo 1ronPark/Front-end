0a1e1e98c1a34e84e98eddd104bc1901f37ba664
hyewonimdang <hyehyekuk202@gmail.com>
2025-08-11 15:20:34 +0900

    [fix] proxy configuration 수정

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/useAuthStore';
 
 const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: '/api',
 });
 
 axiosInstance.interceptors.request.use((config) => {
